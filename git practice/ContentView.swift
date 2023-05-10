bae02dee00b2dc87d38de73839fd2dd6127471b8
이준후 <hprize04@gmail.com>
2023-05-10 10:53:48 +0900

    2번째 추가

--- git practice/ContentView.swift
+++ git practice/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("김도환 바보")
+            Text("1234")
         }
         .padding()
     }
