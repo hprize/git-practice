512c7325d4054ee87bfd01be10880081101894e1
이준후 <hprize04@gmail.com>
2023-05-10 10:16:49 +0900

    Initial Commit

---
+++ git practice/git_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_practiceApp.swift
+//  git practice
+//
+//  Created by 이준후 on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct git_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
